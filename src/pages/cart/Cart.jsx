c777e6a0fb67024bf895c56eed052099ebda7006
shubham <shubhamsagar628@gmail.com>
2024-02-21 18:21:55 +0530

    Modal Popup Created

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout';
 import myContext from '../../context/myContext';
+import Modal from '../../components/modal/Modal';
 
 const Cart = () => {
     const context = useContext(myContext)
@@ -48,12 +49,7 @@ const Cart = () => {
                 </div>
               </div>
               {/* <Modal  /> */}
-              <button
-                type="button"
-                className="w-full  bg-violet-600 py-2 text-center rounded-lg text-white font-bold "
-              >
-                Buy Now
-              </button>
+             <Modal/>
             </div>
           </div>
         </div>
